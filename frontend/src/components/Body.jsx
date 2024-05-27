fe5ad5579fb0769448c41d9a1dc190aff8d30167
Aryan Panchal <panchalaryan698@gmail.com>
2024-05-27 13:08:39 +0530

    link update

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -35,7 +35,7 @@ const Body = () => {
         const formData = new FormData()
         formData.append('image',file)
         try{
-            const response = await axios.post('http://localhost:5000/predict',formData,{
+            const response = await axios.post('https://deepfakedetectionbackend.onrender.com/predict',formData,{
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
